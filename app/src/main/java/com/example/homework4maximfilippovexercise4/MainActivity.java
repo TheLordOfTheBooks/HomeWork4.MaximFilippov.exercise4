b91e7f9b35850163564c28a67a1105d00b7380bb
Maxim <maxim.fil@sisma.org.il>
2024-01-06 22:12:43 +0200

    MainActivity.java

--- app/src/main/java/com/example/homework4maximfilippovexercise4/MainActivity.java
+++ app/src/main/java/com/example/homework4maximfilippovexercise4/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.homework4maximfilippovexercise4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bn;
+    int n;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bn=(Button) findViewById(R.id.clickMe);
+
+    }
+
+
+
+    public void go(View view) {
+        n+=1;
+        if (n!=7)
+            bn.setText("This is a click number:"+ n);
+        else {
+            n = 0;
+            bn.setText("BOOM!");
+        }
     }
 }
